636658d160accf02a85ed34670bbeeb2b57103cb
Kernel <511825269@qq.com>
2017-09-18 17:04:53 +0800

    init

---
+++ Chapter21/ReadFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] dataByte=new byte[200];
+            char[] dataChar=new char[200];
+            try
+            {
+                FileStream aFile = new FileStream("../../Program.cs", FileMode.Open);
+                aFile.Seek(113, SeekOrigin.Begin);
+                aFile.Read(dataByte, 0, 200);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("error has current:" + e.Message);
+            }
+
+            Decoder d = Encoding.UTF8.GetDecoder();
+            d.GetChars(dataByte, 0, dataByte.Length, dataChar, 0);
+            Console.WriteLine(dataChar);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter21/WriteFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WriteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] dataByte;
+            char[] dataChar;
+            try
+            {
+                FileStream aFile = new FileStream("../../test.txt", FileMode.Create);
+                dataChar = "My pink half of the drainpipe.".ToCharArray();
+                dataByte = new byte[dataChar.Length];
+                Encoder e = Encoding.UTF8.GetEncoder();
+                e.GetBytes(dataChar, 0, dataChar.Length, dataByte, 0, true);
+
+                aFile.Seek(0, SeekOrigin.Begin);
+                aFile.Write(dataByte, 0, dataByte.Length);
+
+                Console.WriteLine("write success.");
+                Console.ReadKey();
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine("Error occurred : " + ex.Message);
+            }
+
+            
+        }
+    }
+}
